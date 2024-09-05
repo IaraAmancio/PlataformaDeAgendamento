b4c033ac208b9394bca40ae33795f1ae7a293caa
IaraAmancio <iaraamancio1986@gmail.com>
2024-09-05 19:36:53 -0300

    Adicionando as rotas

---
+++ index.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const personModel = require('./person')
+const sectorModel = require('./sector')
+const reservationModel = require('./reservation')
+
+mongoose.connect('mongodb://localhost:27017/restaurante')
+
+mongoose.model('person', personModel)
+mongoose.model('sector', sectorModel)
+mongoose.model('reservation', reservationModel)
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname,'../Front/views'));
+app.use(express.static(path.join(__dirname, '../Front/public')));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/', require('./router'));
+
+
+// Iniciar o servidor na porta 8080
+app.listen(8080, () => {
+    console.log('Servidor rodando em http://localhost:8080');
+});
